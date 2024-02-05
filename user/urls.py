e0992d609128687e2d6b2e51cb94ee504db89b23
seunghyun.hwang <dragonray@naver.com>
2024-02-05 15:33:41 +0900

    1. 스플래시 라우팅 추가 2. 로그인 기초 라우팅 구성

--- user/urls.py
+++ user/urls.py
@@ -4,4 +4,5 @@ from . import views
 app_name = 'user'
 urlpatterns = [
 	path('', views.index, name = 'index'),
+	path('login/', views.login, name = 'login'),
 ]
--- user/views.py
+++ user/views.py
@@ -1,5 +1,10 @@
 from django.shortcuts import render
 
+
 # Create your views here.
 def index(request):
 	return render(request, 'user/FLW_SP_001.html')
+
+
+def login(request):
+	return render(request, 'user/FLW_OB_001.html')
