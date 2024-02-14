4d38584ef612f7983ce1ae47846fb582a056f859
mksoo <mgs3067@naver.com>
2024-02-14 23:51:25 +0900

    1. bootstrap 커스터마이징을 위해 bootstrap 다운로드 2. 식단 추가 화면 제작

--- yyamyyam/settings.py
+++ yyamyyam/settings.py
@@ -49,6 +49,7 @@ INSTALLED_APPS = [
     "allauth.account",
     "allauth.socialaccount",
     "allauth.socialaccount.providers.kakao",
+    # "django_browser_reload",
 ]
 
 MIDDLEWARE = [
@@ -60,6 +61,7 @@ MIDDLEWARE = [
     "django.contrib.messages.middleware.MessageMiddleware",
     "django.middleware.clickjacking.XFrameOptionsMiddleware",
     "allauth.account.middleware.AccountMiddleware",
+    # "django_browser_reload.middleware.BrowserReloadMiddleware",
 ]
 
 ROOT_URLCONF = "yyamyyam.urls"
