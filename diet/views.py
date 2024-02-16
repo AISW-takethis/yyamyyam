10f5c5e229d29b9a227d681ef6713665e0788718
mksoo <mgs3067@naver.com>
2024-02-16 10:12:36 +0900

    1. template 파일 제작 - 식단 상세, 식단 상세 검색 2. main 섹션에서 스크롤 기능 활성화(스크롤 바는 숨김) 3. bootstrap 색상 수정 4. 로딩바, 날짜 확인 템플릿 추가 5. 네비게이션바 링크 삽입

--- diet/views.py
+++ diet/views.py
@@ -2,8 +2,8 @@ from django.shortcuts import render
 
 
 # Create your views here.
-def recommand(request):
-    return render(request, "diet/recommand.html", {})
+def recommend(request):
+    return render(request, "diet/recommend.html", {})
 
 
 def record_list(request):
@@ -12,3 +12,21 @@ def record_list(request):
 
 def record_add(request):
     return render(request, "diet/record_add.html", {})
+
+
+def record_detail(request):
+    return render(request, "diet/record_detail.html", {})
+
+
+# record_add 페이지를 공유해서 쓰되 전달하는 인자에 차이를 둔다.
+def record_edit(request):
+    return render(request, "diet/record_add.html", {})
+
+
+def record_detail_search(request):
+    item_list = [i for i in range(1, 11)]
+    return render(request, "diet/record_detail_search.html", {"item_list": item_list})
+
+
+def loading(request):
+    return render(request, "loading.html", {})
